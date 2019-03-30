dd9612dc8893eec39634e39a0c6716ef0ebd06da
GlauberC <glaubercarv@gmail.com>
2019-03-30 17:48:43 -0300

    Start course reactjs

---
+++ react/learn-reactjs/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './components/MyInfo/MyInfo.css';
+import MyInfo from './components/MyInfo/MyInfo'
+
+class App extends Component {
+  render() {
+    return (
+      <MyInfo></MyInfo>
+    )
+  }
+}
+
+export default App;
